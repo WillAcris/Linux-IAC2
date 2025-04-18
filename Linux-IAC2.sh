692038d001378c02cbb06afaf9c45fc5000d9742
William Gabriel <80060962+WillAcris@users.noreply.github.com>
2025-04-17 21:03:52 -0400

    Update Linux-IAC2.sh

--- Linux-IAC2.sh
+++ Linux-IAC2.sh
@@ -15,7 +15,7 @@ echo "Instalação do servidor concluída com sucesso!"
 
 
 cd /temp
-wgewt https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
 cp -R * /var/www/html
